ef0688727c5a33e1c20ebd89988cfddaf86f8099
Loukas Pastras <lukaspastras@gmail.com>
2024-10-17 20:49:28 +0300

    added a delete call

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello from node API')
+    res.send('Hello from node API');
 });
 
 app.get('/api/products', async (req, res) =>
@@ -24,17 +24,53 @@ app.get('/api/products', async (req, res) =>
         const products = await Product.find({});
         res.status(200).json(products);
     }catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
 });
 
-app.post('/api/products', async (req, res) =>
+app.get('/api/product/:id', async (req, res) =>
 {
     try{
-        const product = await Product.create(req.body);
-        res.status(200).json(product)
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.put('/api/product/:id', async (req, res) =>
+{
+    try{
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        
+        if (!product){
+            return res.status(404).json({message : "Product not found"});
+        }
+        
+        const updatedProduct = await Product.findById(id);
+
+        res.status(200).json(updatedProduct);
+
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.delete('/api/product/:id', async (req, res) =>
+{
+    try{
+        const { id } = req.params;
+        const product = Product.findByIdAndDelete(id);
+        
+        if (!product){
+            return res.status(404).json({message : "Product not found"});
+        } 
+
+        res.status(200).json({message : "Product Deleted Succesfully"});
+    } catch (error){
+        res.status(500).json({message: error.message});
     }
 });
 mongoose.connect(process.env.MONGO_URI)
