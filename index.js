1d1ebdd9a73522113591e37cdb3006af742570c0
LOUKAS PASTRAS <39627613+loukaspastras@users.noreply.github.com>
2024-10-17 17:59:59 +0300

    halfway through the tutorial

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const express = require('express')
+// @ts-nocheck
+import express from 'express';
+import mongoose from 'mongoose';
+import {Product} from './models/product.model.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express()
+
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -8,3 +17,26 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.send('Hello from node API')
 });
+
+app.get('/api/products', async (req, res) =>
+{
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+app.post('/api/products', async (req, res) =>
+{
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Connected!'))
+.catch(() => console.log('Connection Failed')); 
